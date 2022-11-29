04ecdf1ab811896c04326f0ffc957574c951ccc4
jy2058 <jiy3285@gmail.com>
2022-11-29 22:21:25 +0900

    Bean Validation - groups

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,14 +12,15 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)    // 수정시에만 적용
     private Long id;
-    @NotBlank(message = "공백X")
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     @Range(min = 1000, max = 1000000)
     private Integer price;
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)    // 등록시에만 사용
     private Integer quantity;
 
     public Item() {
